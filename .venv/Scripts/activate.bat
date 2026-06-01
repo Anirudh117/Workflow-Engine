32924af533aca2e05d68ecd4d04c7ab9e3d86951
Anirudhanm07 <anirudhanm55@gmail.com>
2026-06-01 14:52:30 +0530

    Remove venv and add requirements

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Anirudhan\OneDrive\Desktop\Project\tredence-workflow-engine\.venv
+set VIRTUAL_ENV=F:\Project\Tredence-workflow-engine\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
